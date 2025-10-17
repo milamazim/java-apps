f6de89fc864847a76a890d3d7f4f4aa5bcae17ff
Camila Marques <milamazim@gmail.com>
2025-10-16 21:35:34 -0300

    Criado projeto Product

---
+++ product/README.md
@@ -0,0 +1,33 @@
+# Product
+
+Cadastro de produtos padrão, importados ou usados. Para cada tipo de produto é emitido uma etiqueta de preços distinta.
+Exercício elaborado no curso de Java do prof. Nelio Alves.
+
+## Tela do Sistema
+
+```text
+Enter the number of products: 3
+Product #1 data:
+Common, used or imported (c/u/i)? i
+Name: Tablet
+Price: 260.00
+Customs fee: 20.00
+Product #2 data:
+Common, used or imported (c/u/i)? c
+Name: Notebook
+Price: 1100.00
+Product #3 data:
+Common, used or imported (c/u/i)? u
+Name: Iphone
+Price: 400.00
+Manufacture date (DD/MM/YYYY): 15/03/2017
+
+PRICE TAGS:
+Tablet $ 280.00(Customs fee: 20.00)
+Notebook $ 1100.00
+Iphone (used) $ 400.0 (Manufacture date: 15/03/2017)
+```
+
+## Diagrama
+
+![Texto Alternativo](product.png)
---
+++ product/src/application/Program.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+public class Program {
+  public static void main(String[] args) throws ParseException {
+    
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    List<Product> list = new ArrayList<>();
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    System.out.print("Enter the number of products: ");
+    int n = sc.nextInt();
+    sc.nextLine();
+
+    for (int i = 1; i <= n; i++) {
+      System.out.println("Product #" + i + " data:");
+      System.out.print("Common, used or imported (c/u/i)? ");
+      char productType = sc.next().charAt(0);
+      sc.nextLine();
+      System.out.print("Name: ");
+      String name = sc.nextLine();
+      System.out.print("Price: ");
+      double price = sc.nextDouble();
+      sc.nextLine(); 
+
+      if (productType == 'i') {
+        System.out.print("Customs fee: ");
+        double fee = sc.nextDouble();
+        list.add(new ImportedProduct(name, price, fee));
+      }
+      else if (productType == 'u') {
+        System.out.print("Manufacture date (DD/MM/YYYY): ");
+        String date = sc.nextLine();        
+        list.add(new UsedProduct(name, price, sdf.parse(date)));
+      }
+      else {
+        list.add(new Product(name, price));
+      }      
+      
+    }
+
+    System.out.println();
+    System.out.println("PRICE TAGS: ");
+      for (Product prod : list) {
+        System.out.println(prod.priceTag());
+      }
+
+    sc.close();
+
+  }
+
+}
