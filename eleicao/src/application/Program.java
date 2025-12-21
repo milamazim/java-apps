59df924cecf56aeb66bbf7a0c7850ed7ba89acde
Camila Marques <milamazim@gmail.com>
2025-12-21 06:40:30 -0300

    Exercicio de map

---
+++ eleicao/src/application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Program {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);    
+    
+    System.out.print("Enter file full path: ");
+    String path = sc.nextLine();
+
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+      
+      Map<String,Integer> candidateVotes = new HashMap<>();
+      String line = br.readLine();     
+      
+      while(line != null) {
+        String[] fields = line.split(",");
+        String name = fields[0];        
+        Integer votes = Integer.parseInt(fields[1]);                        
+        
+        if (candidateVotes.containsKey(name)) {
+          candidateVotes.put(name, candidateVotes.get(name) + votes);          
+        } else {
+          candidateVotes.put(name, votes);
+        }                           
+        
+        line = br.readLine();       
+
+      }
+
+      for (String key : candidateVotes.keySet()) {
+        System.out.println(key + ": " + candidateVotes.get(key));
+      }
+           
+    
+    } catch(IOException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+
+    sc.close();
+    
+  }
+
+}
